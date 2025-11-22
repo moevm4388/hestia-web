e9def0c62bce4a35f79408a8504e41423e4d9a7a
shelepuginivan <shelepuginivanm@gmail.com>
2025-11-22 11:37:40 +0300

    fix(api): catch all possible exceptions

--- app/api.py
+++ app/api.py
@@ -73,6 +73,11 @@ async def call_function(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail=str(e),
         )
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail=str(e),
+        )
 
 
 @router.get("/health", summary="Проверка состояния сервиса")
